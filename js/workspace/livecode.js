9019be527be2020980d420df06be998d9d88dd4c
Simon <145285958+SimonOkolo@users.noreply.github.com>
2025-09-17 12:11:23 +0100

    Add tabbed interface to LiveCode workspace
    Introduces a tabbed layout in the LiveCode workspace with 'main.py' and 'OUTPUT' tabs. Updates CSS for tab styling and layout, and implements tab switching logic in JavaScript to toggle between content sections.

--- js/workspace/livecode.js
+++ js/workspace/livecode.js
@@ -3,16 +3,63 @@ export class LiveCode {
     this.container = document.getElementById(containerId);
     this.setupViewport();
     this.setupEventListeners();
+    this.switchTabs('translation')
   }
 
   setupViewport() {
     this.container.outerHTML = `
-        <div id="livecode">
+        <section id="livecode">
           <div id="livecode-inner">
-            This is the LiveCode inner section
+            <header>
+              <div id="tab-buttons">
+                <button class="nav-tab" id="tab1-button">main.py</button>
+                <button class="nav-tab" id="tab2-button">OUTPUT</button>
+              </div>
+            </header>
+            <div id="tab-content">
+              <article id="translation">Tab1</article>
+              <article id="output">Tab2</article>
+            </div>
           </div>
-        </div>
+        </section>
     `;
+
+    this.translationTab = document.getElementById('translation') 
+    this.translationTabButton = document.getElementById('tab1-button')
+
+    this.outputTab = document.getElementById('output')
+    this.outputTabButton = document.getElementById('tab2-button')
   }
-  setupEventListeners() {}
+  // Logic to switch between tabs
+  switchTabs(tab) {
+    // Show the selected tab content and update button
+    if (tab === 'translation') {
+      this.translationTab.style.display = 'block';
+      this.translationTab.classList.add('active');
+      this.translationTabButton.style.backgroundColor = '#16161B';
+
+      this.outputTab.style.display = 'none';
+      this.outputTab.classList.remove('active')
+      this.outputTabButton.style.backgroundColor = '#2C3138';
+    } else if (tab === 'output') {
+      this.translationTab.style.display = 'none';
+      this.translationTab.classList.remove('active')
+      this.translationTabButton.style.backgroundColor = '#2C3138';
+
+      this.outputTab.style.display = 'block';
+      this.outputTab.classList.add('active')
+      this.outputTabButton.style.backgroundColor = '#16161B';
+    }
+  }
+
+  setupEventListeners() {
+    // Handle tab button clicks
+    document.querySelectorAll('.nav-tab').forEach((button, index) => {
+      button.addEventListener('click', () => {
+        const tab = index === 0 ? 'translation' : 'output';
+        // Switch to the selected tab
+        this.switchTabs(tab);
+      });
+    });
+  }
 }
